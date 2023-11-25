181a3cc313e0484cdcdf9174ea7226a9ebe9cab1
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2023-11-25 04:39:04 +0000

    Update 5 files

---
+++ src/app/hanoi-towers/hanoi-towers.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hanoi-towers',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hanoi-towers.component.html',
+  styleUrl: './hanoi-towers.component.css'
+})
+export class HanoiTowersComponent {
+
+}
