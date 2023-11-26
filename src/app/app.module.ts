1b8b5b1609671608098420d1f35316e9e1751b4b
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2023-11-26 05:42:26 +0000

    Main Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule                      } from '@angular/core';
 import { BrowserModule                 } from '@angular/platform-browser';
 import { RouterModule                  } from '@angular/router';
 import { AppComponent                  } from './app.component';
+import { HomeComponent                 } from './Modules/home/home.component';
 import { SudokuComponent               } from './Modules/Algorithm/sudoku/sudoku.component';
 //
 const routes = [
-  {  path: 'Sudoku'                , component: SudokuComponent                       },
+  {  path: 'Sudoku' , component: SudokuComponent  },
+  {  path: 'Home'   , component: HomeComponent    },
 ];
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     SudokuComponent,
   ],
   imports: [
@@ -17,7 +20,6 @@ const routes = [
     RouterModule,
     RouterModule.forRoot( routes ),
   ],
-  exports  : [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
