7cbde040450010d3447542a8f45e0a86eee075b3
aperezNWO <57961103+aperezNWO@users.noreply.github.com>
2023-11-30 03:17:30 +0000

    publish test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Modules/home/home.component';
-import { SudokuComponent } from './Modules/Algorithm/sudoku/sudoku.component';
+import { SudokuComponent } from './Modules/Games/sudoku/sudoku.component';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 //
