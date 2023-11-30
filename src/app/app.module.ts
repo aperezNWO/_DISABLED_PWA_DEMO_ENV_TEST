2a0ec5301ef4310271567571f9e256f22715d490
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2023-11-30 02:13:30 +0000

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,26 @@
-import { NgModule                      } from '@angular/core';
-import { BrowserModule                 } from '@angular/platform-browser';
-import { RouterModule                  } from '@angular/router';
-import { AppComponent                  } from './app.component';
-import { HomeComponent                 } from './Modules/home/home.component';
-import { SudokuComponent               } from './Modules/Algorithm/sudoku/sudoku.component';
-import { HttpClient                    } from '@angular/common/http';
-import { HttpClientModule              } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Modules/home/home.component';
+import { SudokuComponent } from './Modules/Algorithm/sudoku/sudoku.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 //
 const routes = [
-  {  path: 'Sudoku' , component: SudokuComponent  },
-  {  path: 'Home'   , component: HomeComponent    },
+  { path: 'Sudoku', component: SudokuComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: '**', component: AppComponent },
 ];
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    SudokuComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, SudokuComponent],
   imports: [
     HttpClientModule,
     BrowserModule,
     RouterModule,
-    RouterModule.forRoot( routes ),
+    RouterModule.forRoot(routes),
   ],
   providers: [HttpClient],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
